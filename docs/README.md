14f0f4fa9a9005e94a91749530732c3dafa88100
tanjilk <tanjilkh@gmail.com>
2021-08-23 15:00:17 +0100

    phathetic tag

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Phatetic tag
+
+### Rename the cheater tag with "pathetic tag"
+
+Download the mod here and drop it into mods folder
---
+++ tag.lua
@@ -0,0 +1,5 @@
+Hooks:Add("LocalizationManagerPostInit", "phatetic", function(loc)
+	LocalizationManager:add_localized_strings({
+		menu_hud_cheater = "phatetic"
+	})
+end)
